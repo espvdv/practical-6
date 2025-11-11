470ef13cb92d812a8a6be2d37a7420ad86f0a326
espvdv <mishahnatienko@gmail.com>
2025-11-11 20:26:26 +0200

    Update math.test.js

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -1,4 +1,4 @@
-const { sum, calculateDiscount } = require('../math.js');
+const { sum, calculateDiscount } = require('../math');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
