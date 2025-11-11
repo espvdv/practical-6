cfda290e5cb90a6f9db98702fd3e6050780417ef
espvdv <mishahnatienko@gmail.com>
2025-11-11 20:35:01 +0200

    Delete math.js

--- math.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function calculateDiscount(price, discount) {
-  return price - (price * discount) / 100;
-}
-
-module.exports = { sum, calculateDiscount };
