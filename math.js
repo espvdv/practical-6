7cf3c6213af34e73399ca6fe1f9b84bdf2c164d6
espvdv <mishahnatienko@gmail.com>
2025-11-11 20:32:36 +0200

    Update math.js

--- math.js
+++ math.js
@@ -1,7 +1,9 @@
 function sum(a, b) {
   return a + b;
 }
+
 function calculateDiscount(price, discount) {
   return price - (price * discount) / 100;
 }
+
 module.exports = { sum, calculateDiscount };
